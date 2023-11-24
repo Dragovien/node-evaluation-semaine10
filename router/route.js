95037e6171f9bbe16a1be0c80167c3498944cf3b
Piilami <kennenislife@gmail.com>
2023-11-24 12:33:50 +0100

    ajout dashboard et changement required

--- router/route.js
+++ router/route.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { register, login } from "../controller/user.controller.js";
+import { dashboard, home, login } from "../controller/user.controller.js";
 
 const router = express.Router();
 
-router.get("/", register);
+router.get("/", home);
 
 router.get("/login", login);
 // router.get('/register', show)
-// router.get('/dashboard', deleteProd)
+router.get("/dashboard", dashboard);
 
 export default router;
