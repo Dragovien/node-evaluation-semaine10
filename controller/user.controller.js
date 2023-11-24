73053b01a6ef174089759fc31cfdf468e0f10f4c
Axel Hégron <axel.hegron@3wa.io>
2023-11-24 16:33:54 +0100

    wip

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,56 +2,63 @@ import userModel from "../model/users.js";
 import CryptoJS from "crypto-js";
 
 const onError = (res) => {
-  return (err) => {
-    console.log("Something broke with DB:");
-    console.log(err.message);
-    res.status(501).json({ message: err.message });
-  };
+    return (err) => {
+        console.log("Something broke with DB:");
+        console.log(err.message);
+        res.status(501).json({ message: err.message });
+    };
 };
 
 export const home = (req, res) => {
-  res.render("user/register", { title: "Register" });
-  return;
+    res.render("user/register", { title: "Register" });
+    return;
 };
 
 export const login = (req, res) => {
-  res.render("user/login", { title: "Login" });
-  const { email, password } = req.body;
-
-  userModel.findOne({ email: email }).then((user) => {
-    if (CryptoJS.SHA256(password).toString() === user.password) {
-      res.status(200);
-      res.redirect("/dashboard");
-    } else {
-      res.status(301);
-      res.redirect("/");
-    }
-  });
-
-  return;
+    res.render("user/login", { title: "Login" });
+    return;
 };
 
+export const userLogin = (req, res) => {
+    const { email, password } = req.body;
+
+    const { method } = req;
+
+    if (method === "POST") {
+        userModel.findOne({ email: email }).then((user) => {
+            if (CryptoJS.SHA256(password).toString() === user.password) {
+                res.status(200);
+                res.redirect("/dashboard");
+            } else {
+                res.status(301);
+                res.render("user/login", {wrongIdentifiers: true,  title: "Register" });
+            }
+        });
+    }
+    return
+}
+
 export const dashboard = (req, res) => {
-  res.render("user/dashboard", { title: "Dashboard" });
-  return;
+    res.render("user/dashboard", { title: "Dashboard" });
+    return;
 };
 
 export const register = async (req, res) => {
-  const { method } = req;
+    const { method } = req;
 
-  if (method === "POST") {
-    try {
-      let user = await userModel.findOne({ email: req.newUser.email });
+    if (method === "POST") {
+        try {
+            let user = await userModel.findOne({ email: req.newUser.email });
 
-      if (user) {
-        res.render("user/register", { existingUser: true, title: "Register" });
-      } else {
-        await userModel.create(req.newUser);
-        res.redirect("./login");
-      }
-    } catch (error) {
-      console.log(error);
+            if (user) {
+                res.render("user/register", { existingUser: true, title: "Register" });
+            } else {
+                await userModel.create(req.newUser);
+                res.redirect("./login");
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
-  }
-  return;
+    return;
 };
--- router/route.js
+++ router/route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {dashboard, home, register, login} from '../controller/user.controller.js'
+import {dashboard, home, register, login, userLogin} from '../controller/user.controller.js'
 import {registerMiddleware} from '../middlewares/registerMiddleware.js'
 import {loginMiddleware} from '../middlewares/loginMiddleware.js'
 const router = express.Router()
@@ -7,6 +7,7 @@ const router = express.Router()
 router.get('/', home)
 router.post('/', registerMiddleware, register)
 router.get('/login', login)
+router.post('/login', userLogin)
 router.get('/dashboard', dashboard)
 
 export default router;
