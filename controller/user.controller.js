34e1c06040e779685e51c6b96de17bb3d66ff02f
Piilami <kennenislife@gmail.com>
2023-11-24 11:58:25 +0100

    ajout login

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,19 +1,21 @@
 // import userModel from '../model/users.js'
 
 const onError = (res) => {
-    return (err) => {
-        console.log('Something broke with DB:')
-        console.log(err.message)
-        res.status(501).json({message: err.message})
-    }
-}
+  return (err) => {
+    console.log("Something broke with DB:");
+    console.log(err.message);
+    res.status(501).json({ message: err.message });
+  };
+};
 
-
-export const home = (req,res) => {
-    res.render('user/home', {title: 'Home'})
-    return
-}
-
+export const register = (req, res) => {
+  res.render("user/register", { title: "Register" });
+  return;
+};
+export const login = (req, res) => {
+  res.render("user/login", { title: "Login" });
+  return;
+};
 // export const all = (req,res) => {
 //     productModel.find({}, {_id: 0, society: 1, qty: 1}).then(products => {
 //         res.render('shop/all', {products, title: "All"})
--- router/route.js
+++ router/route.js
@@ -1,9 +1,11 @@
-import express from 'express'
-import {home} from '../controller/user.controller.js'
-const router = express.Router()
+import express from "express";
+import { register, login } from "../controller/user.controller.js";
 
-router.get('/', home)
-// router.get('/login', all)
+const router = express.Router();
+
+router.get("/", register);
+
+router.get("/login", login);
 // router.get('/register', show)
 // router.get('/dashboard', deleteProd)
 
